362b09f871b47a664e0f76d1ab297d360039d592
motaleb <motaleb@3-devs.com>
2021-01-08 21:36:32 +0600

    ass

--- public/js/app.js
+++ public/js/app.js
@@ -55378,8 +55378,8 @@ var routes = new vue_router__WEBPACK_IMPORTED_MODULE_1__["default"]({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! E:\xampp\htdocs\project\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! E:\xampp\htdocs\project\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! G:\xampp\htdocs\project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! G:\xampp\htdocs\project\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
